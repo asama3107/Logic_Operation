2aaf902eac7884b9ee118e6b9f83bc891e75fed7
asama3107 <asamausuri@gmail.com>
2018-07-05 20:37:30 +0300

    ternarnyj operator

--- src/IfElseOperation.java
+++ src/IfElseOperation.java
@@ -3,7 +3,7 @@
  */
 public class IfElseOperation {
     public void ifElseOperation(String name, int age) {
-        if (name == null || age <= 0) {
+        if (name == null && age <= 0) {
             System.out.println("NOP");
         } else if(name == "SomeName" && age == 30){
 
@@ -11,6 +11,16 @@ public class IfElseOperation {
 
         } else {
             System.out.println("name:" + name + ", age: " + age);
+        }
+// Тернарний оператор
+        String ageResult =(age <= 20 || name == "SomeName") ? "Ok" : name;
+        System.out.println(ageResult);
+
+        if(age == 20) {
+            ageResult = "Ok";
+
+        }else  {
+            ageResult = "name";
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
         IfElseOperation ifElse = new IfElseOperation();
         ifElse.ifElseOperation( "Andrii" , 20);
+        ifElse.ifElseOperation( "Rosemari" , 40);
         ifElse.ifElseOperation( "SomeOne" , 30);
-        ifElse.ifElseOperation( null, 30);
+        ifElse.ifElseOperation( null, -30);
     }
 }
